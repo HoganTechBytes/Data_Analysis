afd3ea76d0b355d39c9be16cdd10a7f9032d47e7
Jon <jonhoganbs@gmail.com>
2026-01-16 12:39:48 -0600

    Added additional sub-query example

--- Parks_Rec/queries/subs.sql
+++ Parks_Rec/queries/subs.sql
@@ -7,3 +7,13 @@ WHERE employee_id IN (
     FROM employee_salary
     WHERE dept_id = 1
 );
+
+SELECT first_name, last_name, salary, AVG(salary)
+FROM employee_salary
+GROUP BY first_name, salary;
+
+SELECT first_name, last_name, salary, (
+    SELECT ROUND(AVG(salary), 2)
+    FROM employee_salary
+) AS average_salary
+FROM employee_salary;
